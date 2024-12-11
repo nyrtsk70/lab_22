971d2687192f6acac798beac2ad601e0c99f96f9
nyrtsk70 <nyrtsk70@gmail.com>
2024-12-11 18:11:58 +0700

    new

---
+++ Source1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bitset>
+#include<cmath>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Ru");          
+    float y, z, x, f, x1, x2, x3;
+    cout << "Вычисление выражения\n";
+    cout << "Введите х=";
+    cin >> x;
+    cout << "Введите у=";
+    cin >> y;
+    cout << "Введите z=";
+    cin >> z;
+    x1 = y - sqrt(abs(x));             
+    x2 = y / (z + (pow(x, 2) / 4));    
+    x3 = x - x2;
+    f = log(abs(x1 * x3));            
+        cout << "Ответ: " << f;//коммент1
+}
